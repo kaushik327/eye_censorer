5be7cd64883c4cb33a87d4d126e0584077bcdec0
Kaushik Varadharajan <kaushik.r.varadharajan@gmail.com>
2022-08-25 16:36:44 -0500

    Initial commit

---
+++ videocapture.py
@@ -0,0 +1,57 @@
+# WORK IN PROGRESS. THE EYE DETECTION IS SCUFFED
+
+import numpy as np
+import cv2
+
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+while 1:
+    ret, img = cap.read()
+    faces = face_cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=5)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        roi = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi, scaleFactor=1.3, minNeighbors=5)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi, (ex,ey), (ex+ew,ey+eh), (0,255,0), 2)
+
+        """
+        if len(eyes) != 2:
+            print('Detected face somehow doesn\'t have two eyes. Not drawing a box')
+        else:
+            # eyes is an array of boxes defined by [x,y,w,h]
+    
+            points = [np.array([ex+ew/2, ey+eh/2]) for (ex,ey,ew,eh) in eyes]
+            p1, p2 = points
+            # The coordinates of the center of each box
+    
+            thickness = sum([ew + eh for (ex,ey,ew,eh) in eyes]) / 8
+            # The thickness of our line should be half the average side length of the boxes.
+    
+            diff = np.subtract(p2, p1)
+            unit = diff / np.linalg.noxrm(diff) * thickness
+            rot_unit = np.array([-unit[1], unit[0]])
+    
+            unit_scale = 1.5
+            rot_unit_scale = 0.7
+    
+            unit *= unit_scale
+            rot_unit *= rot_unit_scale
+    
+            rectangle = [p1 - unit + rot_unit, p1 - unit - rot_unit,
+                         p2 + unit - rot_unit, p2 + unit + rot_unit]
+            rectangle = np.array([[int(a), int(b)] for [a, b] in rectangle])
+            cv2.fillPoly(roi, np.int32([rectangle]), (0,0,0))
+        """
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
